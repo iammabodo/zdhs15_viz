dafff307ea181f900b64834a76fbb8fa3b3d0113
Eltone Mabodo <eltone.mabodo@gmail.com>
2022-05-20 23:59:58 +0200

    Reading in data

--- code/process.R
+++ code/process.R
@@ -1,5 +1,6 @@
 library(tidyverse)
 library(haven)
 
+missing.data.codes <- c(9, 94, 95, 96, 97, 98, 994, 995, 996, 997, 998, 999, 9994, 9995, 9996, 9997, 9998, 9999)
 
-read_stata(data/zdhs_2015.DTA)
+read_dta("data/zdhs_2015.DTA", na = missing.data.codes)
